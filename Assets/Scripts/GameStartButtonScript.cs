f43f88a11f92625d2cff5f7e06107f04716aeebd
FmyFeliz <fmy.feliz.mobile@gmail.com>
2016-03-30 18:58:34 +0900

    hirano_0330_2

---
+++ Assets/Scripts/GameStartButtonScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStartButtonScript : MonoBehaviour {
+
+	// Update is called once per frame
+	public void ToStage1() {
+		Application.LoadLevel ("Stage1");
+	}
+
+}
