8f0bce5a84e79e02a7b04bf235f4b01d70c54087
ds2223 <ds22232223@gmail.com>
2016-03-30 18:29:41 +0900

    itemなど初期バージョン

--- Assets/Scripts/WoodenCrateRed.cs
+++ Assets/Scripts/WoodenCrateRed.cs
@@ -2,10 +2,37 @@
 using System.Collections;
 
 public class WoodenCrateRed : MonoEX {
-
-	// Use this for initialization
+	GameObject gameController;
 	void Start () {
-        Invoke("Destroy", 1);
+		Invoke("Destroy", 1);
+		//Invoke(Item(), 1.3);
+		Item();
+
     }
+	void Item(){
+		int ran = Random.Range (0, 15);			//15分の１の確率でアイテム出現
+		switch (ran){
+		case 1:
+			//var go1 = Resources.Load("Prefab/Item") as GameObject;
+			gameController = GameObject.Find ("GameController");									//クラスの指名
+			GameObject go1 = gameController.GetComponent<GameController> ().Items[0] as GameObject; //コンポーネントの指定
+			Instantiate(go1,gameObject.transform.position,Quaternion.identity);						//プレハブ出力
+			break;
+		case 2:
+			//var go1 = Resources.Load("Prefab/Item") as GameObject;
+			gameController = GameObject.Find ("GameController");
+			GameObject go2 = gameController.GetComponent<GameController> ().Items[1] as GameObject;
+			Instantiate(go2,gameObject.transform.position,Quaternion.identity);
+			break;
+		case 3:
+			//var go1 = Resources.Load("Prefab/Item") as GameObject;
+			gameController = GameObject.Find ("GameController");
+			GameObject go3 = gameController.GetComponent<GameController> ().Items[2] as GameObject;
+			Instantiate(go3,gameObject.transform.position,Quaternion.identity);
+			break;
+		default:;
+			break;
+		}
+	}
 
 }
