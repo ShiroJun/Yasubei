8d12c2cf0dfc8744a87edba7d3e20c4d64e60966
ShiroJun <0909robinson@gmail.com>
2016-03-30 12:55:41 +0900

    ステージ関連のprefabとscript少し追加。

---
+++ Assets/Scripts/StageCreator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StageCreator : MonoBehaviour {
+    public int x = 8;
+    public int z = 8;
+    public GameObject tile;
+    public GameObject BombCenterTile;
+    // Use this for initialization
+    void Start () {
+        TileSet();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void TileSet()
+    {
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < z; j++)
+            {
+                Instantiate(tile, new Vector3(i, 0, j),Quaternion.identity);
+                Instantiate(BombCenterTile, new Vector3(i, 1, j), Quaternion.identity);
+            }
+        }
+    }
+}
