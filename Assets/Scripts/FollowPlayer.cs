56b8220591907ede9c7d8e533707d1980dd886e8
ds2223 <ds22232223@gmail.com>
2016-03-31 18:11:35 +0900

    カメラワークとか

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour
+{
+	public float left;
+	public float right;
+	public Transform target;    // ターゲットへの参照
+
+	void Start ()
+	{
+	}
+
+	void Update ()
+	{
+
+		transform.position = new Vector3(target.position.x, 10, 2);
+		Debug.Log (target.position.x);
+		if(transform.position.x < left){
+			transform.position = new Vector3(left, 10, 2);
+		}
+
+		if(transform.position.x >= right){
+			transform.position = new Vector3(right, 10, 2);
+		}
+	}
+}
