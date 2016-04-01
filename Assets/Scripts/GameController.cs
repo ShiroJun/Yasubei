17d176056eaaca52735eab887b0ca64f048cd5fc
ds2223 <ds22232223@gmail.com>
2016-04-01 16:12:38 +0900

    ゲームコントローラとキャンバスの設定を分けました

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,30 +9,9 @@ public class GameController : MonoBehaviour {
 	public int SpPow;
 	public int Invent;
 
-	public float timer;
-	public Text timerText;
-	public Text ExText;
-	public Text SpText;
-	public Text InvText;
-
 	void Start(){
 	}
 
 	void Update() {
-		CountDown ();
-		ItemPow ();
-	}
-
-	void ItemPow(){
-		ExText.text = "Pow : " + ExPow.ToString();
-		SpText.text = "SP   : " + SpPow.ToString();
-		InvText.text = "Num : " + Invent.ToString();
-	}
-
-	void CountDown(){
-		if (timer > 0) {
-			timer -= 1f * Time.deltaTime;
-			timerText.text = "残り時間 : " + ((int)timer).ToString ();
-		}
 	}
 }
