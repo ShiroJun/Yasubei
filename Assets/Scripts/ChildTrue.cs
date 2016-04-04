7b754d0b7f3043f186816226ca8519964be87da3
ShiroJun <0909robinson@gimail.com>
2016-04-04 19:03:43 +0900

    花火玉貫通バグ修正、ロケット修正。

---
+++ Assets/Scripts/ChildTrue.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChildTrue : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Invoke("TrueCheck",0.05f);
+	}
+	void TrueCheck()
+    {
+            transform.GetChild(1).gameObject.SetActive(true);
+    }
+}
