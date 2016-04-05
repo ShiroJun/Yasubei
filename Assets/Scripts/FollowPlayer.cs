b266360897162b71aa058697e3fd6a98887da30e
ds2223 <ds22232223@gmail.com>
2016-04-05 17:22:35 +0900

    ゴールのtagetに回転と点滅を追加

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -8,6 +8,7 @@ public class FollowPlayer : MonoBehaviour
 	public float up;
 	public float down;
 	public float height;
+	public float angle;
 	public Transform target;    // ターゲットへの参照
 
 	void Start ()
@@ -17,18 +18,17 @@ public class FollowPlayer : MonoBehaviour
 	void Update ()
 	{
 
-		transform.position = new Vector3(target.position.x, height,target.position.z);
+		transform.position = new Vector3(target.position.x, height,target.position.z - angle);
 		if(transform.position.x < left){
 			transform.position = new Vector3(left, height, target.position.z);
 		}
-
 		if(transform.position.x >= right){
 			transform.position = new Vector3(right, height, target.position.z);
 		}
-		if(transform.position.x < up){
+		if(transform.position.z >= up){
 			transform.position = new Vector3(target.position.x, height, up);
 		}
-		if(transform.position.z >= down){
+		if(transform.position.z < down){
 			transform.position = new Vector3(target.position.x, height, down);
 		}
 
---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+	private float Rot;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Rot += (Time.deltaTime * 40);
+		this.transform.rotation = Quaternion.Euler(0,Rot,0);
+	}
+}
