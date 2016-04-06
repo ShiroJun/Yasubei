ccc8349e1e474b27bd8dff7fe837119f35987b4a
ShiroJun <0909robinson@gmail.com>
2016-04-06 11:41:01 +0900

    手形でページ遷移追加

--- Assets/Scripts/GameStartButtonScript.cs
+++ Assets/Scripts/GameStartButtonScript.cs
@@ -1,10 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameStartButtonScript : MonoBehaviour {
+public class GameStartButtonScript : MonoBehaviour
+{
 
-	public void ToStage1() {
-		Application.LoadLevel ("Stage1");
-	}
-
+    public void ToStage1()
+    {
+        Application.LoadLevel("Stage1");
+    }
+    public void ToStage2()
+    {
+        Application.LoadLevel("Stage2");
+    }
+    public void ToStage3()
+    {
+        Application.LoadLevel("Stage3");
+    }
+    public void ToStageBoss()
+    {
+        Application.LoadLevel("BossStage");
+    }
 }
--- Assets/Tegatatest.cs
+++ Assets/Tegatatest.cs
@@ -1,28 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Tegatatest : MonoBehaviour {
+public class Tegatatest : MonoBehaviour
+{
 
     private GameObject tegata;
     private Animator anim;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         tegata = GameObject.Find("Tegata");
         anim = GetComponent<Animator>();
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.name == "Tegata")
+        if (other.gameObject.name == "Tegata1")
         {
-            anim.SetBool("Victory",true);
+            anim.SetBool("Victory", true);
+            Invoke("ToStage3", 3f);
         }
+        if (other.gameObject.name == "Tegata2")
+        {
+            anim.SetBool("Victory", true);
+            Invoke("ToStageBoss", 3f);
+        }
+
+    }
+
+
+    public void ToStage3()
+    {
+        Application.LoadLevel("Stage3");
+    }
+    public void ToStageBoss()
+    {
+        Application.LoadLevel("BossStage");
     }
 }
