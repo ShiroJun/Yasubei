6e2805d24622f9d392c198f53b02c3b082a38060
masanoritakano <masanori77t@gmail.com>
2016-04-07 15:01:50 +0900

    Movietest Scene Web/Android Platform 分け

--- Assets/Scripts/PlayVideo.cs
+++ Assets/Scripts/PlayVideo.cs
@@ -7,24 +7,40 @@ using UnityEngine.SceneManagement;
 
 public class PlayVideo : MonoBehaviour {
 
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+
     public MovieTexture movie;
     private AudioSource audio;
+#endif
+    void Start () {
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+        GetComponent<RawImage>().texture = movie as MovieTexture;
+                audio = GetComponent<AudioSource>();
+                audio.clip = movie.audioClip;
+                movie.Play();
+                audio.Play();
+                           
 
-	void Start () {
-
-        #if UNITY_STANDALONE_WIN
+        if (Application.platform == RuntimePlatform.WindowsEditor)
+        {
             GetComponent<RawImage>().texture = movie as MovieTexture;
             audio = GetComponent<AudioSource>();
             audio.clip = movie.audioClip;
             movie.Play();
             audio.Play();
-        #endif
+        }
+#endif
+#if  UNITY_ANDROID
+       // SceneManager.LoadScene("Title");
+#endif
     }
-
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
     void Update () {
         if (!movie.isPlaying)
         {
             SceneManager.LoadScene("Title");
         }
+
 	}
+#endif
 }
