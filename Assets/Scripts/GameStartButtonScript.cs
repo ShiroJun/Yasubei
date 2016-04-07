d675e26dee7f736a410c57db2f92649345b5d44d
ShiroJun <0909robinson@gimail.com>
2016-04-07 18:42:08 +0900

    ストーリーを適当に追加。

--- Assets/Scripts/GameStartButtonScript.cs
+++ Assets/Scripts/GameStartButtonScript.cs
@@ -20,4 +20,8 @@ public class GameStartButtonScript : MonoBehaviour
     {
         Application.LoadLevel("BossStage");
     }
+    public void ToStory()
+    {
+        Application.LoadLevel("Story");
+    }
 }
