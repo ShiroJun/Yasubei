1b52a828bcc88e33f9b7c1edc61b38b1c36021a8
masanoritakano <masanori77t@gmail.com>
2016-04-07 18:03:18 +0900

    悪代官倒して花火演出

--- Assets/Scripts/FireCreator.cs
+++ Assets/Scripts/FireCreator.cs
@@ -5,11 +5,13 @@ using UnityEngine.SceneManagement;
 public class FireCreator : MonoBehaviour {
 
     public GameObject hanabi;
+    public GameObject hanabi2;
+    public GameObject hanabi3;
 
     void Start()
     {
         StartCoroutine("Bighanabi");
-        Invoke("Ending", 5);
+        Invoke("Ending", 7);
 
     }
     void Update()
@@ -21,14 +23,27 @@ public class FireCreator : MonoBehaviour {
     {
       while (true)
        {
-          Instantiate(hanabi, transform.position, transform.rotation);
-            yield return new WaitForSeconds(2.0f);      
-       } 
+            Hanabipattern();
+            Invoke("Hanabipattern2", 1);
+            Invoke("Hanabipattern3", 0.7f);
+            yield return new WaitForSeconds(2.0f);        
+        } 
                   
     }
     void Ending()
     {
         SceneManager.LoadScene("MovieTest");
     }
-   
+   void Hanabipattern()
+    {
+        Instantiate(hanabi, hanabi.transform.position, transform.rotation);
+    }
+    void Hanabipattern2()
+    {
+        Instantiate(hanabi2, hanabi2.transform.position, transform.rotation);
+    }
+    void Hanabipattern3()
+    {
+        Instantiate(hanabi3, hanabi3.transform.position, transform.rotation);
+    }
 }
