8ec66f86f8f805545671a0c43951c831a6c245f6
ShiroJun <0909robinson@gmail.com>
2016-04-08 09:52:58 +0900

    minigameプロトタイプ追加。

---
+++ Assets/MiniGame/MiniGameSetting.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MiniGameSetting : MonoBehaviour {
+    public float miniGameTime = 50f;
+    public Text miniGameTimeText;
+    public Text miniGameCountDownText;
+    //生まれてくる敵プレハブ
+    public GameObject enemyPrefab;
+    //敵を格納
+    GameObject[] existEnemys;
+    //アクティブ最大数
+    public int maxEnemy = 20;
+    public float time2 = 0;
+    bool start = true;
+    // Use this for initialization
+    void Start()
+    {
+        CountDown3();
+        //配列確保
+        existEnemys = new GameObject[maxEnemy];
+        //周期的に実行したい場合はコルーチン
+        StartCoroutine(Exec());
+
+    }
+
+    void Update()
+    {
+        if (start == false)
+        {
+            time2 += Time.deltaTime;
+            miniGameTimeText.text = "時間：" + time2.ToString("0.0");
+        }
+    }
+
+    //敵を作成する
+    IEnumerator Exec()
+    {
+        while (true)
+        {
+            //time -= Time.deltaTime;
+
+
+            for (int i = 0; i < (time2 / 10); i++)
+            {
+                Generate();
+                if (i == 5)
+                {
+                    break;
+                }
+            }
+            Debug.Log(time2);
+
+            yield return new WaitForSeconds(5.0f);
+        }
+    }
+
+    void Generate()
+    {
+        for (int enemyCount = 0; enemyCount < existEnemys.Length; ++enemyCount)
+        {
+            if (existEnemys[enemyCount] == null)
+            {
+                int pattern = UnityEngine.Random.Range(0, 4);
+                float x = UnityEngine.Random.Range(0, 17);
+                float z = UnityEngine.Random.Range(0, 12);
+                //敵を作成する
+                switch (pattern)
+                {
+                    case 0:
+                        existEnemys[enemyCount] = Instantiate(enemyPrefab, new Vector3(x, 1f, 0), transform.rotation) as GameObject;
+                        break;
+                    case 1:
+                        existEnemys[enemyCount] = Instantiate(enemyPrefab, new Vector3(x, 1f, 11), transform.rotation) as GameObject;
+                        break;
+                    case 2:
+                        existEnemys[enemyCount] = Instantiate(enemyPrefab, new Vector3(0, 1f, z), transform.rotation) as GameObject;
+                        break;
+                    case 3:
+                        existEnemys[enemyCount] = Instantiate(enemyPrefab, new Vector3(16, 1f, z), transform.rotation) as GameObject;
+                        break;
+                }
+                return;
+            }
+        }
+    }
+
+    void CountDown3()
+    {
+        miniGameCountDownText.text = "参";
+        Invoke("CountDown2", 1f);
+    }
+    void CountDown2()
+    {
+        miniGameCountDownText.text = "弐";
+        Invoke("CountDown1", 1f);
+    }
+    void CountDown1()
+    {
+        miniGameCountDownText.text = "壱";
+        Invoke("CountDown0", 1f);
+    }
+    void CountDown0()
+    {
+        miniGameCountDownText.text = "始め！";
+        Invoke("CountDownGo", 1f);
+    }
+    void CountDownGo()
+    {
+        start = false;
+
+    }
+}
+
---
+++ Assets/Scripts/TextUP.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextUP : MonoBehaviour {
+    public GameObject pos;
+	// Use this for initialization
+	void Start () {
+        Invoke("ToTitle",30f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.y <= pos.transform.position.y)
+        {
+            transform.position = new Vector3(pos.transform.position.x, transform.position.y + 0.8f, pos.transform.position.z);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            ToTitle();
+        }
+       
+	}
+    void ToTitle()
+    {
+        Application.LoadLevel("Title");
+    }
+}
