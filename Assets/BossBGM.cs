b46d1eb36eda133c9633ab20b621d728bd804e9b
masanoritakano <masanori77t@gmail.com>
2016-04-09 18:21:44 +0900

    ボス面BGM変更　Joystick画像差し替え

---
+++ Assets/BossBGM.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossBGM : MonoBehaviour {
+
+    public GameObject gamecon;
+    private AudioSource audio;
+
+	void Start () {
+        //gamecon = GameObject.Find("GameController");
+        audio = gamecon.GetComponent<AudioSource>();
+        audio.mute = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
